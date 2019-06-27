6891824d2c053adf84f25c56dd08ddd89b7f7223
John <wzhen0930@163.com>
2019-06-27 18:19:06 +0800

    [WN] 安装types提示node 新增zlib库gzip压缩

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
+const qs = require('querystring')
+const zlib = require('zlib')
 
 http.createServer(function (request, response) {
     const body = []
+    const parsedUrl = url.parse(request.url,true,true)
+    console.log(parsedUrl)
+    console.log(qs.stringify({a:[1,3]}))
     request.on('data', function (chunk) {
         body.push(chunk)
     })
     request.on('end', function () {
         response.writeHead(200, {
             'Content-Type': 'text/plain;utf-8',
+            'Content-Encoding':'gzip',
         })
-        response.write(Buffer.concat(body).toString())
+        response.write(zlib.gzipSync(Buffer.concat(body).toString()))
         response.end()
     })
 })
@@ -24,12 +31,16 @@ function sendData() {
         hostname: 'localhost',
         port: 1314,
         method: 'POST',
+        path:'/a/v?a=1',
     }, function (response) {
         const ret = fs.createWriteStream('./ret.txt', {
             flags: 'w',
             mode: 0o777,
         })
-        response.pipe(ret)
+        if (response.headers['content-encoding' !== 'gzip']){
+            return response.pipe(ret)
+        }
+        return response.pipe(zlib.createGunzip()).pipe(ret)
     })
 
     request.write('aaa')
